9bf594c5bd8ae68e4dac7cededf7bd2aeedad6e7
Aaquib Niaz <aaquibniaz3600@gmail.com>
2018-10-26 20:59:23 +0530

    configured static and media setting

--- bookcafe/urls.py
+++ bookcafe/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from .import settings
+from django,confs.urls.static import static
 
 from . import views
+
 urlpatterns = [
     path("",views.home,name="home"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
