00e5f337cbc0a0742421d91d864577a6ed1d1d8f
dilip\ jat <dilipjat98@gmail.com>
2018-11-17 03:46:31 +0530

    till notifications list view completed but detailed view showing some problem

--- notification/views.py
+++ notification/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Notification
+from accounts.models import User
 
 class NotificationList(generic.ListView):
     """here we have all the notifications for a specific user"""
-    models = Notification
-    template_name = 
+    template_name = 'notification/home.html'
+    context_object_name = 'notifications'
+    def get_queryset(self):
+        return Notification.objects.filter(recipient = self.request.user).order_by('timestamp')
+
+class NotificationDetail(generic.DeleteView):
+    template_name = 'notification/detail.html'
+    context_object_name = 'notice'
+    def get_queryset(self,**kwargs):
+        return Notification.objects.filter(id=kwargs['pk'])
