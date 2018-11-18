7c70273c70b6de57252025c967df53cb0d7e7c2a
dilip\ jat <dilipjat98@gmail.com>
2018-11-18 20:18:18 +0530

    token expire code compeleted and it sending notifications

--- tok/admin.py
+++ tok/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import token
 
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('token','user_name','book_name','deleted')
+    date_hierarchy = 'date'
 
 
-admin.site.register(token)
+admin.site.register(token,TokenAdmin)
