ac8a26d681aed95aa97952d97c068e8b4c62d0e2
Aaquib Niaz <aaquibniaz3600@gmail.com>
2018-11-27 20:41:06 +0530

    half integrated to page

--- tag/views.py
+++ tag/views.py
@@ -1,31 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.template.loader import render_to_string
 import json
 
 # Create your views here.
 from django.http import HttpResponse
 from django.views import View
 from .models import Tag
-from .forms import  SearchTagForm
 from book.models import Book
 
 class SearchTag(View):
     """ Search tags with autocomplete (live search) """
 
     def get(self, request):
-        form = SearchTagForm()
-        context = {'searchtag' : form}
+        context = {}#'form' : form}
         return render(request, 'tag/search_tags.html', context)
 
     def post(self,request):
         q = request.POST['q']
         print("qery =",q)
-        form = SearchTagForm()
-        #tags = Tag.objects.filter(title__icontains=q)
         tags = Book.objects.filter(title__icontains=q)
         print(tags)
-        context = {'tags' : tags, 'searchtag' : form}
+        context = {'tags' : tags}#, 'form' : form}
         return render(request, 'tag/search_tags.html', context)
 
 class TagJson(View):
@@ -38,7 +32,7 @@ class TagJson(View):
         tags = Book.objects.filter(title__icontains=q)
 
         for tag in tags:
-            new = {'q' : tag.title, 'count' : 1}
+            new = {'q' : tag.title}
             taglist.append(new)
         return HttpResponse(json.dumps(taglist), content_type="application/json")
 
