c303780cac6377a3936533a7dfd79b7d415af764
dilip\ jat <dilipjat98@gmail.com>
2018-12-11 20:25:35 +0530

    email sending for notification  is working properly

--- cronta/models.py
+++ cronta/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from notification.models import Notification
 
 class EmailQueue(models.Model):
 	mail_to = models.CharField(max_length = 200)
@@ -14,3 +17,12 @@ class EmailQueue(models.Model):
 
 	def __unicode__(self):
 		return "[%s] %s" % (self.mail_to, self.mail_subject)
+
+@receiver(post_save,sender = Notification)
+def addmail(sender,**kwargs):
+	if kwargs['created']:
+		EmailQueue.objects.create(
+			mail_to = kwargs['instance'].recipient,
+			mail_subject = kwargs['instance'].notification_title,
+			mail_body = kwargs['instance'].description,
+		)
